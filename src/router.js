46b598870c78c07abb90f3c1d884a53982b4d5ba
정규성 <jeong-gyuseong@jeong-gyuseong-ui-noteubug.local>
2024-07-23 00:12:35 +0900

    로그인관련

--- src/router.js
+++ src/router.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const randomSearchRoute = require('./function/serch/randomSearch');
 const searchRoute = require('./function/serch/search');
+const apiUserController = require('./api/user/controller');
+const authenticateToken = require('./middleware/authenticate');
 
 const router = express.Router();
 
 router.use('/random-search', randomSearchRoute);
 router.use('/search', searchRoute);
+
+
+
+
+use(authenticateToken);
+router.get('/api/user/my', apiUserController.show);
+router.post('/api/user/my', apiUserController.update);
+
 
 module.exports = router;
